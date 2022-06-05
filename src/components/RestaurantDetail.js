be43eb475f9555a3abaf7738259ba661b447f1c8
murphyvica <murphyv@uw.edu>
2022-06-05 03:58:48 -0700

    Reconfigured Firebase, and added Firebase db

--- src/components/RestaurantDetail.js
+++ src/components/RestaurantDetail.js
@@ -1,11 +1,15 @@
 import { useParams } from 'react-router-dom';
 
+import { getDatabase, ref, set } from 'firebase/database';
+
 export function RestaurantDetail() {
 
     const urlParams = useParams();
 
     // Use lodash here to filter by the restaurant name param to include details
     console.log(urlParams);
+
+    const database = getDatabase();
 
     return(
         <h2> Description of restaurant </h2>
