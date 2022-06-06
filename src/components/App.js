f82cd27fceaeca16eb7bf480d41b9df0157f4ab0
murphyvica <murphyv@uw.edu>
2022-06-06 01:33:17 -0700

    Started on Add feature

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import { NavigationBar } from './NavigationBar.js';
 import { RestuarantList } from './RestaurantList.js';
 import { SearchBar } from './SearchForm.js';
@@ -11,11 +11,27 @@ import { Routes, Route } from 'react-router-dom';
 import { Navigate } from "react-router-dom";
 import { AddPage } from './AddRestaurant.js';
 import { FavoriteRestaurant } from './Favorite.js';
-//import restaurant from './data/restaraunts.json';
+import restaurant from './data/restaraunts.json';
 
 export default App;
 
 function App(props) {
+
+  const [restaurantArray, setRestaurantArray] = useState(restaurant);
+
+  const addRestaurant = (name, address, type, description) => {
+    const newRestaurant = {
+      name : name,
+      address : address,
+      type : type,
+      description : description
+    }
+
+    const newRestaurantArray = [...restaurantArray, newRestaurant];
+    setRestaurantArray (newRestaurantArray);
+    
+  } 
+
   return (
     <div className="container-fluid search text-light px-0 pt-5">
       <NavigationBar />
